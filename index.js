fe25efe23f8456bdc70da895dc520691a042b29c
Ritesh <ritesh@synup.com>
2017-09-20 19:51:04 +0530

    started working on the task

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  function checkValidTime(time) {
+    var hours = Number(time.match(/^(\d+)/)[1]),
+        minutes = Number(time.match(/:(\d+)/)[1]),
+        AMPM = time.match(/(\D\D)$/)[1],
+        sHours, sMinutesl;
+
+    AMPM = AMPM.toLowerCase();
+    if(AMPM == "pm" && hours<12) hours = hours+12;
+    if(AMPM == "am" && hours==12) hours = hours-12;
+    sHours = hours.toString();
+    sMinutes = minutes.toString();
+    if(hours<10) sHours = "0" + sHours;
+    if(minutes<10) sMinutes = "0" + sMinutes;
+    return(sHours + ":" + sMinutes);  
+  }
+
+	function showTime() {
+		console.log('hi');
+	}
+
+  $('input.timepicker').timepicker({ 
+  	timeFormat: 'h:mm p' 
+  });
+});
